bc6245617d93de9ad98dfb18427bb8a104b2bbe0
Andy Stevens <57344820+apstevens@users.noreply.github.com>
2020-12-03 19:58:52 +0000

    Added additional quote objects

--- js/script.js
+++ js/script.js
@@ -27,8 +27,71 @@ const quotes = [
     type: "Warrior"
   },
   {
+    quote: "Every victorious warrior draws his strength from the highest source; his love.",
+    source: "Tapan Ghosh",
+    citation: "https://www.brainyquote.com/topics/warrior-quotes",
+    year: "2018",
+    type: "Warrior"
+  },
+  {
+    quote: "Victorious warriors win first and then go to war, while defeated warriors go to war first and then seek to win.",
+    source: "Sun Tzu",
+    citation: "https://www.brainyquote.com/topics/warrior-quotes",
+    year: "2018",
+    type: "Warrior"
+  },
+  {
+    quote: "The true soldier fights not because he hates what is in front of him, but because he loves what is behind him.",
+    source: "G.K. Chesterton",
+    citation: "https://www.brainyquote.com/topics/warrior-quotes",
+    year: "2018",
+    type: "Warrior"
+  },
+  {
+    quote: "The warrior stands, ready to defend, ready to fight for those he serves. He is a warrior of virtue, A knight of conscience. He is who we all wish to be.",
+    source: "Anthony T. Hincks",
+    citation: "https://www.brainyquote.com/topics/warrior-quotes",
+    year: "2018",
+    type: "Warrior"
+  },
+  {
     quote: "To err is human; to forgive is divine.",
     source: "Alexander Pope",
+    citation: "https://www.brainyquote.com/topics/forgiveness-quotes",
+    year: "2019",
+    type: "Forgiveness"
+  },
+  {
+    quote: "To forgive is to set a prisoner free and discover that the prisoner was you.",
+    source: "Lewis B. Smedes",
+    citation: "https://www.brainyquote.com/topics/forgiveness-quotes",
+    year: "2019",
+    type: "Forgiveness"
+  },
+  {
+    quote: "Never forget the three powerful resources you always have available to you: love, prayer, forgiveness.",
+    source: "H. Jackson Brown, Jr",
+    citation: "https://www.brainyquote.com/topics/forgiveness-quotes",
+    year: "2019",
+    type: "Forgiveness"
+  },
+  {
+    quote: "Acceptance and tolerance and forgiveness, those are life-altering lessons.",
+    source: "Jessica Lange",
+    citation: "https://www.brainyquote.com/topics/forgiveness-quotes",
+    year: "2019",
+    type: "Forgiveness"
+  },
+  {
+    quote: "It's not an easy journey, to get to a place where you forgive people. But it is such a powerful place, because it frees you.",
+    source: "Tyler Perry",
+    citation: "https://www.brainyquote.com/topics/forgiveness-quotes",
+    year: "2019",
+    type: "Forgiveness"
+  },
+  {
+    quote: "We think that forgiveness is weakness, but it's absolutely not; it takes a very strong person to forgive..",
+    source: "T.D. Jakes",
     citation: "https://www.brainyquote.com/topics/forgiveness-quotes",
     year: "2019",
     type: "Forgiveness"
@@ -39,7 +102,35 @@ const quotes = [
     citation: "https://www.brainyquote.com/topics/wisdom-quotes",
     year: "2016",
     type: "Wisdom"
-  }
+  },
+  {
+    quote: "If you don't know where you are going, any road will get you there.",
+    source: "Lewis Carroll",
+    citation: "https://www.brainyquote.com/topics/wisdom-quotes",
+    year: "2016",
+    type: "Wisdom"
+  },
+  {
+    quote: "Give me a lever long enough and a fulcrum on which to place it, and I shall move the world.",
+    source: "Archimedes",
+    citation: "https://www.brainyquote.com/topics/wisdom-quotes",
+    year: "2016",
+    type: "Wisdom"
+  },
+  {
+    quote: "The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails.",
+    source: "William Arthur Ward",
+    citation: "https://www.brainyquote.com/topics/wisdom-quotes",
+    year: "2016",
+    type: "Wisdom"
+  },
+  {
+    quote: "It is not what happens to you, but how you react to it that matters.",
+    source: "Epictetus",
+    citation: "https://www.brainyquote.com/topics/wisdom-quotes",
+    year: "2016",
+    type: "Wisdom"
+  },
 
 ];
 
